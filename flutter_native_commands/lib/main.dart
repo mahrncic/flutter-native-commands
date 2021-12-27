0958f985068f129c6ee1ea987a9677e4c6bed567
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-27 23:31:46 +0100

    Implemented Java code for fetching battery level from the device.

--- flutter_native_commands/lib/main.dart
+++ flutter_native_commands/lib/main.dart
@@ -42,8 +42,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _getBatteryLevel();
   }
 
   @override
